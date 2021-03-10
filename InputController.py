b6527e105daff402a9a278c42f4536d40dbd55a8
nobtobthelob <jgroen888@gmail.com>
2021-03-10 18:09:13 -0500

    input controller functions

---
+++ InputController.py
@@ -0,0 +1,26 @@
+
+# displays the input menu to the user such as courses taken
+# course preferences, credits preferred etc, should include save button
+def displayInput():
+
+
+
+
+# saves all of the courses taken and other information
+def saveInput():
+
+
+# to be called in save input
+def saveCourses():
+
+
+# saves other schedule preferences
+def savePreferences():
+
+
+# returns array of courses taken
+def getCourses():
+
+
+# returns array of other schedule preferences
+def getPreferences():
